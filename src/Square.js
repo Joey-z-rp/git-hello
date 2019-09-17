3a1d95fdbac80f441fa6f704ca8c4a8b13558df5
Joey <joey.z.rp@gmail.com>
2019-09-17 18:33:11 +1000

    Use state

--- src/Square.js
+++ src/Square.js
@@ -1,14 +1,24 @@
 import React from 'react';
 
-class Square extends React.Component {  
+class Square extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+		  	value: null,
+		};
+	}
+
 	render() {
 		return (
-			<button className="square" onClick={() => alert('click')}>
-				{this.props.value}
+			<button
+				className="square"
+				onClick={() => this.setState({ value: 'X' })}
+			>
+				{this.state.value}
 			</button>
 		);
 	}
 }
 
   export default Square;
-  
