2b7d4d42359d59128b1484a2db73aff31395ca99
Joey <joey.z.rp@gmail.com>
2019-09-18 21:58:08 +1000

    Lift state up

--- src/Board.js
+++ src/Board.js
@@ -2,35 +2,53 @@ import React from 'react';
 import Square from './Square';
 
 class Board extends React.Component {
-    renderSquare(i) {
-      return <Square value={i} />;
-    }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			squares: Array(9).fill(null),
+		};
+	}
 
-  export default Board;
-  
+	handleClick(i) {
+		const squares = [...this.state.squares];
+		squares[i] = 'X';
+		this.setState({ squares });
+	}
+
+	renderSquare(i) {
+		return (
+			<Square
+				onClick={() => this.handleClick(i)}
+				value={this.state.squares[i]}
+			/>
+		);
+	}
+
+	render() {
+		const status = 'Next player: X';
+
+		return (
+			<div>
+				<div className="status">{status}</div>
+				<div className="board-row">
+					{this.renderSquare(0)}
+					{this.renderSquare(1)}
+					{this.renderSquare(2)}
+				</div>
+				<div className="board-row">
+					{this.renderSquare(3)}
+					{this.renderSquare(4)}
+					{this.renderSquare(5)}
+				</div>
+				<div className="board-row">
+					{this.renderSquare(6)}
+					{this.renderSquare(7)}
+					{this.renderSquare(8)}
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Board;
+	
